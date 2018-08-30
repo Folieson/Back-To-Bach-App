bca181795791dd7011d301c642c887f37924d85f
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2018-08-30 20:26:19 +0500

    finished app

--- Back To Bach App/ViewController.swift
+++ Back To Bach App/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player = AVAudioPlayer()
+    var timer = Timer()
+    
+    @IBOutlet var volumeSlider: UISlider!
+    @IBOutlet var positionSlider: UISlider!
+    
+    @IBAction func play(_ sender: UIBarButtonItem) {
+        player.prepareToPlay()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        player.play()
+    }
+    @IBAction func pause(_ sender: UIBarButtonItem) {
+        player.pause()
+    }
+    @IBAction func stop(_ sender: UIBarButtonItem) {
+        player.stop()
+        timer.invalidate()
+        positionSlider.value = 0.0
+        player.currentTime = 0.0
+    }
+    @IBAction func volumeSliderMoved(_ sender: UISlider) {
+        player.volume = volumeSlider.value
+    }
+    @IBAction func positionSliderMoved(_ sender: UISlider) {
+        player.currentTime = Double(positionSlider.value)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioPath = Bundle.main.path(forResource: "bach-bwv140-bisotti", ofType: "mp3")
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+        } catch {
+            // Process any errors
+        }
+        positionSlider.maximumValue = Float(player.duration)
+        positionSlider.value = 0.0
+    }
+    
+    @objc func updateTime () {
+        positionSlider.value = Float(player.currentTime)
     }
 
     override func didReceiveMemoryWarning() {
